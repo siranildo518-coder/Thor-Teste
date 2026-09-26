79bde3b83836d3bb96d94dfb3953e94bd6e378db
siranildo518-coder <siranildo518@gmail.com>
2026-09-26 13:30:13 -0300

    Atualiza cache do login no THOR TESTE

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v151');
+importScripts('./sw-core-thor7.js?v=thor-teste-v156-login');
